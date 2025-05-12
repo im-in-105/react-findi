71016a66a224a86143d4ae98aaea1a46a3ff1169
im-in-105 <146203551+im-in-105@users.noreply.github.com>
2025-05-13 02:15:20 +0900

    Revert "cra->vite change"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 function App() {
-  return (
-    <div className="App">
-      안녕하세요
-    </div>
-  );
+    const [message, setMessage] = useState('')
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/hello')
+            .then(res => {
+                setMessage(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, [])
+
+    return (
+        <div>
+            <h1>SpringBoot에서 온 메시지:</h1>
+            <p>{message}</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
