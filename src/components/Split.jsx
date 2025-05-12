7ac0a94afcb884bdc959e21123297414aed198b4
im-in-105 <146203551+im-in-105@users.noreply.github.com>
2025-05-13 02:27:19 +0900

    Reapply "cra->vite change"
    This reverts commit 71016a66a224a86143d4ae98aaea1a46a3ff1169.

---
+++ src/components/Split.jsx
@@ -0,0 +1,11 @@
+// Split.js
+import React from 'react';
+import '../styles/Split.css';
+
+function Split() {
+    return (
+        <div className="split"></div>
+    );
+}
+
+export default Split;
